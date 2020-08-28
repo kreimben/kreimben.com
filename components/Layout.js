1efc37ae99d24d16b44190f6ddf816c434ec547c
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-29 01:00:53 +0900

    Change design

--- components/Layout.js
+++ components/Layout.js
@@ -6,11 +6,20 @@ export default function Layout(props) {
     return (
         <div>
             <Head>
-                <title> { props.title } </title>
+                <title> {props.title} </title>
             </Head>
+
+            <header className="text-center">
+                <div className="w-full flex justify-center py-8">
+                    <img src="/images/kreimben_memoji.jpeg" className="rounded-full flex justify-center w-40 h-40"></img>
+                </div>
+                <p className="text-2xl font-thin pb-4">Kreimben.com, Indie developer's website</p>
+            </header>
+
             <Menu />
+
             <main>
-                { props.children }
+                {props.children}
             </main>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,6 @@ import Layout from '../components/Layout'
 export default function Home() {
     return (
         <>
-            <header className="text-center">
-                <div className="w-full flex justify-center py-8">
-                    <img src="/images/kreimben_memoji.jpeg" className="rounded-full flex justify-center w-40 h-40"></img>
-                </div>
-                <p className="text-2xl font-thin pb-4">Kreimben.com, Indie developer's website</p>
-            </header>
             <Layout title="Kreimben.com">
                 <main>
                     <h1 className="">Home page</h1>
