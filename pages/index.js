0ad8ad64e8f9be318de9602451b360db351d0605
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-29 00:40:40 +0900

    .

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 export default function Home() {
     return (
         <>
-            <header className="container ">
+            <header className="h-40 text-center bg-blue-400">
                 <p>Hello, kreimben!</p>
             </header>
             <Layout title="Kreimben.com">
