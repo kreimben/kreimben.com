c414534b2b83ec19396596a8b244b010a5faf884
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-28 23:48:32 +0900

    Import main tag

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,16 @@ import Head from 'next/head'
 
 import Menu from '../components/Menu';
 
-export default function Layout( { children } ) {
+export default function Layout({ children }) {
     return (
         <div>
             <Head>
                 <title>Welcome to kreimben.com!</title>
             </Head>
             <Menu />
-            { children }
+            <main>
+                {children}
+            </main>
         </div>
     )
 }
