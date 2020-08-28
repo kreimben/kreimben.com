5421e4236627305c49cd10ae51c2366fee6f6e3a
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-29 00:02:24 +0900

    Remove redundant things

--- components/Menu.js
+++ components/Menu.js
@@ -5,7 +5,7 @@ const elementStyle = "text-blue-800 font-extrabold text-xl text-center flex-grow
 export default function Menu() {
     return (
         <div className="bg-gray-300 items-center flex py-4">
-            <Link href="/" className=""><a className={elementStyle}>Home</a></Link>
+            <Link href="/"><a className={elementStyle}>Home</a></Link>
             <Link href="/aboutme"><a className={elementStyle}>About me</a></Link>
             <Link href="/blog"><a className={elementStyle}>Blog</a></Link>
             <Link href="/apps"><a className={elementStyle}>Apps</a></Link>
