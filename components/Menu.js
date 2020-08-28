658759d50d1eb041d2a7b6e55c36b4495e48443c
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-28 23:46:24 +0900

    Initial commit

--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 
-const elementStyle = "text-red-200";
+const elementStyle = "text-blue-800 font-extrabold text-xl text-center flex-grow";
 
 export default function Menu() {
     return (
-        <div className="bg-red-300 items-center flex">
-            <Link href="/"><a className={elementStyle}>Home</a></Link>
+        <div className="bg-gray-300 items-center flex py-4">
+            <Link href="/" className=""><a className={elementStyle}>Home</a></Link>
             <Link href="/aboutme"><a className={elementStyle}>About me</a></Link>
             <Link href="/blog"><a className={elementStyle}>Blog</a></Link>
+            <Link href="/apps"><a className={elementStyle}>Apps</a></Link>
         </div>
     )
 }
