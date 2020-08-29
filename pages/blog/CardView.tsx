88b22e41f6c08680cf0464708bba6eed33aafc37
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-29 14:09:55 +0900

    Fix typo

--- pages/blog/CardView.tsx
+++ pages/blog/CardView.tsx
@@ -5,6 +5,6 @@ type blogParams = { title: string, date: Date };
 const CardView: React.FunctionComponent<blogParams> = ({ title, date }) => (
     <div className="bg-gray-500" >
         <p className="text-red-700 text-lg" > title: {title} </p>
-        < p > date: {date} </p>
+        <p>date: {date}</p>
     </div>
 );
