659f221cccfe736e407c60c625b26f3fac8e1a1f
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-30 23:28:47 +0900

    Add shadow

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout(props) {
 
             <header className="text-center">
                 <div className="w-full flex justify-center py-8">
-                    <img src="/images/kreimben_memoji.jpeg" className="rounded-full flex justify-center w-40 h-40"></img>
+                    <img src="/images/kreimben_memoji.jpeg" className="rounded-full flex justify-center w-40 h-40 shadow-2xl"></img>
                 </div>
                 <p className="text-2xl font-thin pb-4">Kreimben.com, Indie developer's website</p>
             </header>
