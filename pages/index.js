254044de678eb94191fe4b972daef06c9c1823ee
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-31 00:28:31 +0900

    Implement home elements

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Layout from '../components/Layout'
 
-import {loremIpsum} from '../components/loremipsum'
-
 export default function Home() {
     return (
         <>
             <Layout title="Kreimben::Home">
-                <main>
-                    <h1 className="">Home page</h1>
-                    <p> { loremIpsum } </p>
-                    <p> { loremIpsum } </p>
+                <main className="flex justify-center">
+                    <div className="w-4/5 py-32 mt-6 shadow-xl rounded-lg bg-gray-300 flex justify-center">
+                        <p className="font-semibold text-6xl">iOS, macOS, and anything!</p>
+                    </div>
                 </main>
             </Layout>
         </>
