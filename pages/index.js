f6e00659d5a6d4d2059e94ee3a0026aff23af293
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-08-31 00:33:12 +0900

    Import dummy data for test layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Layout from '../components/Layout'
 
+import {loremIpsum} from '../components/loremipsum'
+
 export default function Home() {
     return (
         <>
             <Layout title="Kreimben::Home">
                 <main className="flex justify-center">
-                    <div className="w-4/5 py-32 mt-6 shadow-xl rounded-lg bg-gray-300 flex justify-center">
+                    <div className="w-4/5 py-32 my-6 shadow-xl rounded-lg bg-gray-300 flex justify-center">
                         <p className="font-semibold text-6xl">iOS, macOS, and anything!</p>
                     </div>
                 </main>
+                    <p>{loremIpsum}</p>
+                    <p>{loremIpsum}</p>
             </Layout>
         </>
     )
