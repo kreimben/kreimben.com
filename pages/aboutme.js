ccaa20ad8ce0f8849fd77dd57f94c6f42910423e
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-09-06 11:53:46 +0900

    Add some phrases

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -7,14 +7,14 @@ export default function AboutMe() {
                 <div className="bg-gray-300 w-4/5 rounded-lg my-16 p-8 inline-block shadow-xl text-center font-mono">
                     <p className="text-3xl">Hello! I'm iOS, macOS and Web developer!</p>
                     <br />
-                    <p className="font-serif text-xl">I've been programming whatever I can express my-self.</p>
+                    <p className="font-serif text-xl">I've been programming whatever I can express my-self since I was high school student.</p>
                 </div>
             </div>
 
             <div className="flex justify-center">
 
-                <div className="bg-gray-300 w-5/6 rounded-lg my-16 p-8 inline-block shadow-xl text-center text-5xl">
-                    안녕하세요!
+                <div className="bg-gray-300 w-4/5 rounded-lg mb-16 p-12 inline-block shadow-xl text-center text-3xl font-serif">
+                    I want to post many of things about programming and anything I'm interested!
                 </div>
                 
             </div>
