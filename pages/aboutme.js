2e6d065b78382d89b7730de7a33c543917e7b891
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-09-06 11:50:32 +0900

    Change design and Add some phrases

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -3,7 +3,22 @@ import Layout from '../components/Layout'
 export default function AboutMe() {
     return (
         <Layout title="Kreimben::AboutMe" isHome={false}>
-            <h1>This is me</h1>
+            <div className="flex justify-center" key="mainContent">
+                <div className="bg-gray-300 w-4/5 rounded-lg my-16 p-8 inline-block shadow-xl text-center font-mono">
+                    <p className="text-3xl">Hello! I'm iOS, macOS and Web developer!</p>
+                    <br />
+                    <p className="font-serif text-xl">I've been programming whatever I can express my-self.</p>
+                </div>
+            </div>
+
+            <div className="flex justify-center">
+
+                <div className="bg-gray-300 w-5/6 rounded-lg my-16 p-8 inline-block shadow-xl text-center text-5xl">
+                    안녕하세요!
+                </div>
+                
+            </div>
+
         </Layout>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,17 @@ export default function Home() {
     return (
         <>
             <Layout title="Kreimben::Home" isHome={true}>
+                <div className="flex justify-center text-center">
+                    <div className="bg-gray-300 p-8 m-12 rounded-lg w-4/5 text-4xl font-serif">
+                        Welcome to indie developer's website!
+                    </div>
+                </div>
                 <main className="flex justify-center">
-                    <div className="w-4/5 py-32 my-6 shadow-xl rounded-lg bg-gradient-to-r from-teal-300 to-blue-500 flex justify-center">
+                    <div className="w-4/5 py-32 mb-12 shadow-xl rounded-lg bg-gradient-to-r from-teal-300 to-blue-500 text-center">
+                        <p className="text-4xl font-serif">I code</p>
                         <p className="font-semibold text-6xl">iOS, macOS, and anything!</p>
                     </div>
                 </main>
-                <div className="flex justify-center" key="mainContent">
-                    <div className="bg-gray-300 w-4/5 rounded-lg m-8 p-4 inline-block shadow-xl text-center font-mono">
-                        <p className="text-3xl">Hello! I'm iOS, macOS and Web developer!</p>
-                        <br />
-                        <p className="font-serif text-xl">I've been programming whatever I can express my-self.</p>
-                    </div>
-                </div>
-
-                <div className="bg-gray-300 rounded-lg m-8 p-4 inline-block shadow-xl text-center">
-                    <p>{loremIpsum}</p>
-                    <p>{loremIpsum}</p>
-                </div>
             </Layout>
         </>
     )
