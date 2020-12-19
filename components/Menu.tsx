1e53d1f2d3a3d28ea140af929b947ca0499c3189
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-12-20 05:56:51 +0900

    Delete js files

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,7 +6,6 @@ const Menu = () => {
     return (
         <nav className="bg-gray-300 items-center flex py-4 divide-x divide-gray-500 sticky top-0 shadow-lg">
             <Link href="/"><a className={elementStyle}>Home</a></Link>
-            <Link href="/aboutme"><a className={elementStyle}>About me</a></Link>
             <Link href="/blog"><a className={elementStyle}>Blog</a></Link>
             <Link href="/apps"><a className={elementStyle}>Apps</a></Link>
         </nav>
