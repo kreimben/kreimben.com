32891b577aa1c1a6d813dce865af9aa156f54cf5
Aksidion Kreimben <aksidionkreimben@gmail.com>
2021-04-05 19:49:07 +0900

    Before install alpha version.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <Menu current="this is value"/>
+    <div>Hello, {{ name }}</div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+import Menu from '../components/Menu.vue';
+
+export default defineComponent({
+    components: {
+        Menu
+    },
+    data() {
+        return { 
+            name: "Aksidion Kreimben!"
+        }
+    },
+});
 </script>
