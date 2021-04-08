b66ef78dfb98af83cab3911074f4f05948b2fb62
Aksidio Kreimben <aksidionkreimben@gmail.com>
2021-04-08 16:55:07 +0900

    Delete default data base

--- Backend/app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6068f87da4489ac0abbf98c86bca2eb6'),
-    },
-  },
-});
--- Backend/app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'c755f859-0016-479e-8e26-08afc8520526'
-};
