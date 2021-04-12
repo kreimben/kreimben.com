581bef5b35d1a8141b035988ce5d5ee29e4a24a6
Aksidion Kreimben <aksidionkreimben@gmail.com>
2021-04-12 17:58:06 +0900

    Implement categories view. (with out fetching photo)

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,30 +1,38 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-cols v-for="(cate, index) in this.categories" :key="index">
-                <v-card class="text-center ma-4 pa-4" lg="4">
-                    {{ cate }}
-                </v-card>
-
-            </v-cols>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        md="4"
+        lg="4"
+        xl="4"
+        v-for="(category, index) in this.categories"
+        :key="index"
+      >
+        <v-card class="text-center ma-4 py-16">
+          <v-card-title
+            class="justify-center mb-6 font-weight-black"
+            style="font-size: 2.5rem"
+            >{{ category.name }}</v-card-title
+          >
+          <v-card-subtitle class="lightgray--text">{{ category.description }}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
 
-import { FetchCategories } from '../api/FetchCategories';
+import { FetchCategories } from "../api/FetchCategories";
 
-@Component({}) 
+@Component({})
 export default class CategoriesView extends Vue {
+  categories = null;
 
-    categories = null;
-
-    async mounted(): Promise<any> {
-        this.categories = await FetchCategories();
-    }
-
-
+  async mounted(): Promise<any> {
+    this.categories = await FetchCategories();
+  }
 }
 </script>
