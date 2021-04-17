1e714a84e5257346f0644af667dcb8849d976d8f
Aksidion Kreimben <aksidionkreimben@gmail.com>
2021-04-17 18:11:27 +0900

    Use middle ware method (.use)

--- server.ts
+++ server.ts
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (requst, response) => {
-    response.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/', (requst, response) => {
+//     response
+//         .set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
+//         .sendFile(path.join(__dirname + '/dist/'));
+// });
+
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen('8080', () => {
     console.log(`Server starts with ${process.env.PORT}`);
