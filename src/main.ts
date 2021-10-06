f9a24a88a1a2aa8c5759222aac9f9c2ee17f19e8
Aksidion <aksidionkreimben@gmail.com>
2021-10-06 17:34:35 +0900

    Remove redundancy.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import { VueReCaptcha } from "vue-recaptcha-v3";
-
-Vue.use(VueReCaptcha, { siteKey: "6Le_mKQaAAAAAKg_BhsrG8YOeHGHf_63ytWLzA6Q" });
 
 new Vue({
   router,
@@ -17,17 +13,5 @@ new Vue({
     return {
       token: null,
     };
-  },
-  methods: {
-    async recaptcha() {
-      // (optional) Wait until recaptcha has been loaded.
-      await this.$recaptchaLoaded();
-
-      // Execute reCAPTCHA with action "login".
-      const token = await this.$recaptcha("login");
-
-      // Do stuff with the received token.
-      console.log(`token is ${token}`);
-    },
   },
 }).$mount("#app");
