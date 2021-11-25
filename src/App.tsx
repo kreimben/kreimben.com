93b230eb33f8d9a9e95222bf1d3c501dba332e29
Aksidion Kreimben <aksidion@kreimben.com>
2021-11-26 00:03:53 +0900

    Install TailWindCss and SolidJS. Implement Basic layout of Website.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+
+    console.log("App Started!");
+
+    return (
+        <div>
+            <p1>Hello!</p1>
+            <p1>Welcome to kreimben.com!</p1>
+        </div>
+    );
+};
+
+export default App;
