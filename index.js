0cfb22985e6c0d497013884e7d431f731296b84d
Kreimben <aksidion@kreimben.com>
2022-01-12 18:41:04 +0900

    Refactoring.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import * as insta from "./src/insta";
+//import * as insta from "./src/insta";
 
-function mainInit(): void {
+function mainInit() {
     alert("load main page!");
+    console.log("load main page!");
 
-    insta.getPosts();
+    //insta.getPosts();
 
     const app = document.getElementById("App");
     app.innerHTML="<b1>Hello world!</b1>";
