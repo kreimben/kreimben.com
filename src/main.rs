ac9d5d05182ff2b7c81c34c99d0409890c029433
Aksidion Kreimben <aksidion@kreimben.com>
2022-01-28 23:05:42 +0900

    Upgrade rocket rc version and Remove redundant codes.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 mod server;
 mod instagram;
 
-#[tokio::main]
-async fn main() {
+fn main() {
 
-    let server = server::Server::new("5000").await.unwrap();
-    let _result = server.run_server().await;
-
-    //let insta = instagram::Instagram::new();
-    //insta.get_posts().await;
 }
