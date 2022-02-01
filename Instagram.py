79ab7c4f0f56289bc24b2716f793f44462aeb4fb
Aksidion Kreimben <aksidion@kreimben.com>
2022-02-01 10:14:27 +0900

    Before branch to webhook and image server.

--- Instagram.py
+++ Instagram.py
@@ -1,26 +1,10 @@
 import requests
 from logging import debug, info, error
-import sys
 
 INSTAGRAM_TOKEN = ""
 
-
-def get_user_id():
-    global INSTAGRAM_TOKEN
-
-    with open("./.instagram_token", encoding="utf-8") as f:
-        INSTAGRAM_TOKEN = f.readline()
-
-    fields = "id,username"
-    url = "https://graph.instagram.com/me?fields={}&access_token={}".format(fields, INSTAGRAM_TOKEN)
-    result = requests.get(url=url)
-    print("result: ", result.json())
-
-    if not result.json()["error"]:
-        return result.json()["id"]
-    else:
-        print("Error!: ", result.json()["error"]["message"])
-        return sys.exit()
+with open("./.instagram_token", encoding="utf-8") as f:
+    INSTAGRAM_TOKEN = f.readline()
 
 
 def get_user_media():
@@ -29,7 +13,8 @@ def get_user_media():
     fields = "id" # caption(hashtag)
     url = "https://graph.instagram.com/me/media?fields={}&access_token={}".format(fields, INSTAGRAM_TOKEN)
     result = requests.get(url=url)
-    debug("get user media result: ", result.json())
+    print(url)
+    print("get user media result: ", result.json())
 
     return result.json()
 
@@ -54,3 +39,15 @@ def get_media_children(child_id: str):
     debug("child result: ", result.json())
 
     return result.json()
+
+
+# Use this method ONLY!!!
+def get_photo_url():
+    media = get_user_media()
+    urls = []
+    for e in media["data"]:
+        e_id = get_representative_media_id(e["id"])
+        datas = get_media_children(e_id)["data"]
+        for data in datas:
+            print("data: ", data)
+            urls.append(data["media_url"])
--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 # Custom.
 from configure import *
 from event import setting_event
-import Instagram
 
 # Setting base app.
 app = FastAPI()
@@ -19,14 +17,9 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(req: Request):
-    media = Instagram.get_user_media()
+
+    # Load saved image from image server.
     urls = []
-    for e in media["data"]:
-        e_id = Instagram.get_representative_media_id(e["id"])
-        datas = Instagram.get_media_children(e_id)["data"]
-        for data in datas:
-            print("data: ", data)
-            urls.append(data["media_url"])
 
     r = {
         "request": req,
