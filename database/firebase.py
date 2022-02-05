06ccacd3830daf309320bde69464c44584290308
Aksidion Kreimben <aksidion@kreimben.com>
2022-02-05 18:11:42 +0900

    Update await/async

--- database/firebase.py
+++ database/firebase.py
@@ -26,9 +26,9 @@ def read_posts():
     return results
 
 
-def write_post(post_id: str, param: dict):
+async def write_post(post_id: str, param: dict):
     c = _get_client()
-    c.collection('post').document(post_id).set(param)
+    await c.collection('post').document(post_id).set(param)
 
 
 def _get_client():
