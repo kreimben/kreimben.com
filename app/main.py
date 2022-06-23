baa75602d2256205af6a16537b38a68e8102c357
Aksidion Kreimben <aksidion@kreimben.com>
2022-06-23 19:09:41 +0900

    Deal with router. (frontend)

--- app/main.py
+++ app/main.py
@@ -1,21 +1,13 @@
-# FastAPI.
-from fastapi.responses import FileResponse
-
-# Custom.
 from configure import *
 from event import setting_event
-import os
+import frontend
 import uvicorn
 
 # Setting base app.
 app: FastAPI = ready()
 setting_event(app)
 
-
-@app.get("/")
-async def entry():
-    return {"success": True, "content": "How about seek other APIs?"}
-
+app.include_router(frontend.router)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=10120, reload=True, log_level="info")
