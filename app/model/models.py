8555706399b157d7950edc204639661e2b1bee54
Aksidion Kreimben <aksidion@kreimben.com>
2022-06-27 02:19:47 +0900

    Add new models (Authorization).

--- app/model/models.py
+++ app/model/models.py
@@ -29,8 +29,16 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(String(25), nullable=False, unique=True, primary_key=True)
-    email = Column(String(100), nullable=True, unique=True)
+    email = Column(String(100), nullable=True)
     first_name = Column(String(20), nullable=False)
     last_name = Column(String(20), nullable=False)
     created_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
     thumbnail_url = Column(String(100), nullable=True)
+    authorization = Column(String(10), ForeignKey('authorizations.name'))
+
+
+class Authorization(Base):
+    __tablename__ = 'authorizations'
+
+    uuid = Column(String(20), nullable=False, unique=True, primary_key=True)
+    name = Column(String(10), nullable=False, unique=True)
