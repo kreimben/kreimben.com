89b3499b76d751a6fd82bcd053df75d4462b72f8
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-24 23:14:43 +0900

    Start with Django!

--- app/utils/env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-def get_secret_key() -> str:
-    return os.environ.get('OAUTH2_SECRET_KEY')
--- test/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-
-from .main import app
-
-client = TestClient(app)
-
-def test_read_main():
-    response = app.get("/")
-    assert response.status_code == "200"
