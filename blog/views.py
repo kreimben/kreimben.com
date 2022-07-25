cf87eccdfd9c9464810aeac0065e529f3ab8c363
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-25 20:29:10 +0900

    Implement detail view.

--- blog/views.py
+++ blog/views.py
@@ -13,5 +13,14 @@ class BlogView(TemplateView):
         return self.render_to_response(context)
 
 
-def index(request):
-    return HttpResponse('Hi, It\'s Kreimben\'s Blog.')
+class BlogPostDetailView(DetailView):
+    template_name = '../templates/blog/blog_post.html'
+    model = Post
+
+    def get(self, request, **kwargs):
+        post = Post.objects.filter(id=kwargs['post_id']).first()
+        print(post.categories.name)
+        context = {
+            'post': post
+        }
+        return self.render_to_response(context)
