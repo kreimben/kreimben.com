e9943ce808a572c8db67160c473faef639704e01
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-25 13:53:05 +0900

    Change models.

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,31 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        get_latest_by = 'created_at'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    subtitle = models.CharField(max_length=200)
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subtitle = models.CharField(max_length=200, null=True)
+    categories = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     content = models.TextField(default='')
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+    class Meta:
+        get_latest_by = ['title', 'created_at']
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
