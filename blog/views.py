ad31bdded4d422e5ffc4275a6ff6e040d90b9e9c
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-01 06:02:04 +0900

    Add filtering with category.

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,34 @@
 from django.core.paginator import Paginator
-from django.http import HttpRequest, HttpResponseRedirect
+from django.db.models import Q
+from django.http import HttpRequest
 from django.views.generic import TemplateView, DetailView
 
-from .models import Post
+from .models import Post, Category
 
 
 class BlogView(TemplateView):
     template_name = '../templates/blog/blog.html'
 
     def get(self, request: HttpRequest, **kwargs):
-        posts = Post.objects.order_by('-created_at').filter(status='published').all()
+        posts = ''
+        category = request.GET.get('category')
+        if category is not None:
+            posts = Post.objects.order_by('-created_at') \
+                .filter(Q(category__name=category) & Q(status='published')) \
+                .all()
+            print(f'posts: {posts}')
+        else:
+            posts = Post.objects.order_by('-created_at').filter(status='published').all()
         page = Paginator(posts, 15)
+
+        categories = Category.objects.order_by('name').all()
 
         page_number = request.GET.get('page')
         page_obj = page.get_page(page_number)
 
-        if page_number is None or page_number != str(page_obj.number):
-            return HttpResponseRedirect('?page=1')
-
         context = {
-            'page_obj': page_obj
+            'page_obj': page_obj,
+            'categories': categories
         }
         return self.render_to_response(context)
 
