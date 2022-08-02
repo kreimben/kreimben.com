7cfdb7b2cb559fdd5bf094acf26637f0b64a95ae
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-03 02:15:45 +0900

    Add STATICFILES_DIRS

--- kreimben_com/settings.py
+++ kreimben_com/settings.py
@@ -159,9 +159,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+if not DEBUG:
+    STATICFILES_DIRS = (
+        '/app/static/',
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
