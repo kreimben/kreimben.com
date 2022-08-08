4375c9090d70a0eab3f3120e2bc81d590eefc8e1
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-08 14:30:25 +0900

    Collect static.

--- static/admin/js/autocomplete.js
+++ static/admin/js/autocomplete.js
@@ -21,15 +21,13 @@
         return this;
     };
 
-    $(function() {
+    $(function () {
         // Initialize all autocomplete widgets except the one in the template
         // form used when a new formset is added.
         $('.admin-autocomplete').not('[name*=__prefix__]').djangoAdminSelect2();
     });
 
-    $(document).on('formset:added', (function() {
-        return function(event, $newFormset) {
-            return $newFormset.find('.admin-autocomplete').djangoAdminSelect2();
-        };
-    })(this));
+    document.addEventListener('formset:added', (event) => {
+        $(event.target).find('.admin-autocomplete').djangoAdminSelect2();
+    });
 }
