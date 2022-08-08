5a297af039b238d41c6e86d388c9845bf2664f62
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-08 14:41:49 +0900

    Change static dir.

--- kreimben_com/settings.py
+++ kreimben_com/settings.py
@@ -154,7 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static_ready'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
