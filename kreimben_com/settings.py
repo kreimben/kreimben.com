8e05e1ef54440a5ac57231dd25e42c95676b406d
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-27 20:20:41 +0900

    collectstatic

--- kreimben_com/settings.py
+++ kreimben_com/settings.py
@@ -150,8 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / 'media'
