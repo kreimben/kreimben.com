88a80a2ccaea876c937c76b000552349c7d9ee2f
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-10 18:04:03 +0900

    Adjust cache on view.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import BlogPostDetailView, BlogView, PostSearchView, BlogFileDownloadCounterView
 
 urlpatterns = [
-    path("", BlogView.as_view(), name="blog"),
-    path("post/<int:post_id>/", BlogPostDetailView.as_view(), name="post"),
-    path('post/<int:post_id>/to/<str:ip>/download/<int:file_id>/', BlogFileDownloadCounterView.as_view(), name='file_download'),
+    path("", cache_page(60 * 60)(BlogView.as_view()), name="blog"),
+    path("post/<int:post_id>/", cache_page(60 * 60)(BlogPostDetailView.as_view()), name="post"),
+    path('post/<int:post_id>/to/<str:ip>/download/<int:file_id>/', BlogFileDownloadCounterView.as_view(),
+         name='file_download'),
     path("search/", PostSearchView.as_view(), name="search"),
 ]
