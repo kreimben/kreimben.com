93f92dc66ddb71f7612e090668e177defed85377
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-19 10:17:20 +0900

    Adjust new cache policy.

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from home.views import HomeView
 
-urlpatterns = [path("", HomeView.as_view(), name="home")]
+urlpatterns = [path("", cache_page(3600 * 6)(HomeView.as_view()), name="home")]
