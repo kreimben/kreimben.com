888f8bc3ff0e5df0ee3e4822e92774b101b65b4d
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-25 21:34:13 +0900

    Remove per_page cache.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from .views import BlogPostDetailView, BlogView, PostSearchView, BlogFileDownloadCounterView
 
 urlpatterns = [
-    path("", cache_page(3600 * 6)(BlogView.as_view()), name="blog"),
-    path("post/<int:post_id>/", cache_page(3600 * 6)(BlogPostDetailView.as_view()), name="post"),
+    path("", BlogView.as_view(), name="blog"),
+    path("post/<int:post_id>/", BlogPostDetailView.as_view(), name="post"),
     path('post/<int:post_id>/to/<str:ip>/download/<int:file_id>/', BlogFileDownloadCounterView.as_view(),
          name='file_download'),
     path("search/", PostSearchView.as_view(), name="search"),
