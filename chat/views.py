2a66e8ae0328c2a5dc929cea1cb340100611ff66
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-31 19:38:41 +0900

    Fix urls.

--- chat/views.py
+++ chat/views.py
@@ -7,4 +7,7 @@ class ChatView(TemplateView):
     template_name = '../templates/chat/chat.html'
 
     def get(self, request, *args, **kwargs):
-        return self.render_to_response({'websocket_endpoint': os.getenv('WEBSOCKET_ENDPOINT')})
+        return self.render_to_response({
+            'websocket_endpoint': os.getenv('WEBSOCKET_ENDPOINT'),
+            'ip_geo_url': os.getenv('IP_GEO_URL')
+        })
