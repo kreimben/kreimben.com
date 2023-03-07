5915ea22f53f3d91ddb3a3c38b33dd93bdac8e39
Aksidion Kreimben <aksidion@kreimben.com>
2023-03-07 13:21:05 +0900

    Fix models

--- chat/models.py
+++ chat/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Chatter(models.Model):
     hashed_value = models.CharField(max_length=8)
     ip_address = models.GenericIPAddressField()
-    country = models.CharField(max_length=100)
-    region_name = models.CharField(max_length=1000)
-    city = models.CharField(max_length=1000)
-    timezone = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    region_name = models.CharField(max_length=1000, null=True, blank=True)
+    city = models.CharField(max_length=1000, null=True, blank=True)
+    timezone = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
