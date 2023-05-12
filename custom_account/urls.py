1d22cc85e680d5a446adfdb0d7b81ab6ff1f220a
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-12 13:32:38 +0900

    Fix login view.

--- custom_account/urls.py
+++ custom_account/urls.py
@@ -1,9 +1,10 @@
 from allauth.socialaccount.providers.google.views import oauth2_callback, oauth2_login
 from django.urls import path
 
-from .views import CustomLogoutView
+from .views import CustomLogoutView, CustomLoginView
 
 urlpatterns = [
+    path("login/", CustomLoginView.as_view(), name='account_login'),
     path("logout/", CustomLogoutView.as_view(), name='account_logout'),
     path('google/login/', oauth2_login, name='google_login'),
     path('google/login/callback/', oauth2_callback, name='google_callback'),
