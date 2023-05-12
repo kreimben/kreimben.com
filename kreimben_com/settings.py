5c6231683cba66530af5faed16c2a1c91d286aeb
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-12 12:28:02 +0900

    Implement logout overwritten view.

--- kreimben_com/settings.py
+++ kreimben_com/settings.py
@@ -96,7 +96,10 @@ ROOT_URLCONF = "kreimben_com.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, 'templates/allauth'),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -231,3 +234,4 @@ SOCIALACCOUNT_PROVIDERS = {
 
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_SIGNUP_REDIRECT_URL = '/'
+ACCOUNT_ALLOW_SIGNUPS = False
