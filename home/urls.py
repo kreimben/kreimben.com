02b7ac13df17547fde55acfd4ddbef353a526774
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-12 20:24:03 +0900

    Remove cache for view.

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from home.views import HomeView, ProfileView
 
 urlpatterns = [
-    path("", cache_page(3600 * 6)(HomeView.as_view()), name="home"),
+    path("", HomeView.as_view(), name="home"),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
