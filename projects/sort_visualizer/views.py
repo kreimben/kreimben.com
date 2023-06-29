7f7cc7391370c7e64adab4fdfaf4d1ee9e9d0648
Aksidion Kreimben <aksidion@kreimben.com>
2023-06-29 21:51:37 +0900

    Implement loading sortings from DB.

--- projects/sort_visualizer/views.py
+++ projects/sort_visualizer/views.py
@@ -1,5 +1,12 @@
 from home.views import BaseTemplateView
+from projects.sort_visualizer.models import Sort
 
 
 class SortVisualizerView(BaseTemplateView):
     template_name = 'sort_visualizer/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Sort Visualizer'
+        context['sorting'] = Sort.objects.all()
+        return context
