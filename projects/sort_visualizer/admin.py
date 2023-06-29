47b6900da6216cb533aad06ff7c19bf0dc50f98d
Aksidion Kreimben <aksidion@kreimben.com>
2023-06-29 21:43:06 +0900

    Implement sort model.

--- projects/sort_visualizer/admin.py
+++ projects/sort_visualizer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from projects.sort_visualizer.models import Sort
+
+
+@admin.register(Sort)
+class SortAdmin(admin.ModelAdmin):
+    list_display = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'average_case', 'code')
+    list_filter = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'average_case', 'code')
+    search_fields = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'average_case', 'code')
+    ordering = ['name']
--- projects/sort_visualizer/models.py
+++ projects/sort_visualizer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Sort(models.Model):
+    name = models.CharField(max_length=100)
+    time_complexity = models.CharField(max_length=100)
+    space_complexity = models.CharField(max_length=100)
+    best_case = models.CharField(max_length=100)
+    worst_case = models.CharField(max_length=100)
+    average_case = models.CharField(max_length=100)
+    code = models.TextField()
+
+    class META:
+        ordering = ['name']
