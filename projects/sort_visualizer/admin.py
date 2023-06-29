e71b99df3a5434ea426a9653ca6374ce5b2ea139
Aksidion Kreimben <aksidion@kreimben.com>
2023-06-29 22:49:02 +0900

    Fix fields.

--- projects/sort_visualizer/admin.py
+++ projects/sort_visualizer/admin.py
@@ -5,7 +5,7 @@ from projects.sort_visualizer.models import Sort
 
 @admin.register(Sort)
 class SortAdmin(admin.ModelAdmin):
-    list_display = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'average_case', 'code')
-    list_filter = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'average_case', 'code')
-    search_fields = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'average_case', 'code')
+    list_display = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'code', 'method')
+    list_filter = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'code', 'method')
+    search_fields = ('name', 'time_complexity', 'space_complexity', 'best_case', 'worst_case', 'code', 'method')
     ordering = ['name']
