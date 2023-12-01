2ee34bcc7daf6a7baf8f7b1923484c3b9548efec
Aksidion Kreimben <aksidion@kreimben.com>
2023-12-01 21:21:37 +0900

    Remove allauth related and custom account related

--- custom_account/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomAccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'custom_account'
--- custom_account/urls.py
+++
@@ -1,7 +0,0 @@
-from allauth.socialaccount.providers.google.views import oauth2_callback, oauth2_login
-from django.urls import path
-
-urlpatterns = [
-    path('google/login/', oauth2_login, name='google_login'),
-    path('google/login/callback/', oauth2_callback, name='google_callback'),
-]
