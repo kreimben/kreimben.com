7f4df4e954e66bc0ab4e7ca0dc4661de2395f24f
Jehwan Kim <jehwan.kim@kreimben.com>
2024-02-13 18:59:29 +0900

    Remove PostSearchView from blog URLs
    The PostSearchView import and its corresponding path were removed from the blog URLs. Additionally, the name of the BlogPostDetailView URL has been simplified to "post_detail" for clarity.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import BlogPostDetailView, BlogView, PostSearchView, BlogFileDownloadCounterView
+from .views import BlogPostDetailView, BlogView, BlogFileDownloadCounterView
 
 urlpatterns = [
     path("", BlogView.as_view(), name="blog"),
-    path("post/<int:post_id>/", BlogPostDetailView.as_view(), name="post"),
+    path("post/<int:post_id>/", BlogPostDetailView.as_view(), name="post_detail"),
     path('post/<int:post_id>/to/<str:ip>/download/<int:file_id>/', BlogFileDownloadCounterView.as_view(),
          name='file_download'),
-    path("search/", PostSearchView.as_view(), name="search"),
+    # path("search/", PostSearchView.as_view(), name="search"),
 ]
