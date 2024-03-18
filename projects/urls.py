3358995799240d3ce1221bf228fecc625c9d8989
Jehwan Kim <jehwan.kim@kreimben.com>
2024-03-19 07:38:24 +0900

    Add URL path for mnist_classifier in projects
    A URL path for the newly created "mnist_classifier" project has been added in the "projects/urls.py" file. This enables the routing towards the specific project, improving the repository structure and overall navigation.

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.ProjectsView.as_view(), name='projects'),
     path('image_to_ascii_art/', include('projects.image_to_ascii_art.urls')),
     path('sort_visualizer/', include('projects.sort_visualizer.urls')),
+    path('mnist_classifier/', include('projects.mnist_classifier.urls'))
 ]
